13d60e7d87d1bf1d31d5e0e255374eee71c7a844
jaco <jaco.com>
2017-09-20 15:13:43 +1000

    wip

--- nodejs/NoteApp/app.js
+++ nodejs/NoteApp/app.js
@@ -4,13 +4,35 @@ const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
+const titleOptions = {
+        describe: "Title of note",
+        demand: true,
+        alias: "t"
+      }
+
+const bodyOptions = {
+        describe: "Body of note",
+        demand: true,
+        alias: "b"
+      }
+
 const argv = yargs
-    .command('add','Add a new note',{})
+    .command('add','Add a new note',{
+      title: titleOptions,
+      body: bodyOptions
+    })
+    .command('list','List notes')
+    .command('read','Read a note',{
+      title: titleOptions
+    })
+    .command('remove','Remove a note',{
+      title: titleOptions
+    })
     .help()
     .argv;
 var command = argv._[0];
-console.log('Command: ', command);
-console.log('Yargs', argv);
+// console.log('Command: ', command);
+// console.log('Yargs', argv);
 
 debugger;
 
---
+++ nodejs/NoteApp/playground/arrow-functio.js
@@ -0,0 +1,11 @@
+var square = x => x * x
+console.log(square(9))
+
+var user = {
+	name: 'jaco',
+	sayHi: () => {
+		console.log(`hi`)
+	}
+}
+
+user.sayHi();

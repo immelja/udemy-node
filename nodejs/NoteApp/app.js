0a80e4ba8c1d5850a58b16a82acc47cec275ad83
jaco <jaco.com>
2017-09-20 14:30:54 +1000

    wip

--- nodejs/NoteApp/app.js
+++ nodejs/NoteApp/app.js
@@ -1,12 +1,13 @@
-console.log('Starting app.js');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-const argv = yargs.argv;
+const argv = yargs
+    .command('add','Add a new note',{})
+    .help()
+    .argv;
 var command = argv._[0];
 console.log('Command: ', command);
 console.log('Yargs', argv);

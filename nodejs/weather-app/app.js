76362929be553ca66c2e8c2d8b81a5dcf841a9e3
jaco <jaco.com>
2017-09-21 05:30:31 +1000

    m

--- nodejs/weather-app/app.js
+++ nodejs/weather-app/app.js
@@ -1,8 +1,41 @@
 const request = require('request')
+const yargs = require('yargs')
+
+const argv = yargs
+.options({
+	address: {
+		demand: true,
+		alias: 'a',
+		describe: 'Address to fetch weather for',
+		string: true
+	}
+})
+.help()
+.alias('help','h')
+.argv
+
+var encodedAddress = encodeURIComponent(argv.address)
+console.log(encodedAddress)
 
 request({
-	url: 'https://maps.googleapis.com/maps/api/geocode/json?address=28%20covala%20court%20st%20helena',
+	url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
 	json: true
 }, (error, response, body) => {
-    console.log(body)
+	if (error) {
+		console.log('unable to connect')
+	} else if (body.status === 'ZERO_RESULTS') {
+		console.log('unable to find address')
+	} else if (body.status === 'OK') {
+    // console.log(JSON.stringify(body, undefined, 2))
+    console.log(`Address: ${body.results[0].formatted_address}`)
+    console.log(`Lat: ${body.results[0].geometry.location.lat}`)
+    console.log(`Long: ${body.results[0].geometry.location.lng}`)
+}
 })
+
+
+// request({
+// 	url: 'http://www.gogle.com.au/?gws_rd=ssl'
+// }, (error, response, body) => {
+//     console.log(JSON.stringify(error, undefined, 2))
+// })

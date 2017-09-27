21c5bacb3f4506e31fb43c7ad61cfd99bc9cebad
jaco <jaco.com>
2017-09-27 19:10:48 +1000

    start testing

--- nodejs/node-web-server/server.js
+++ nodejs/node-web-server/server.js
@@ -50,6 +50,12 @@ app.get('/about',(req,res) => {
 	})
 })
 
+app.get('/projects',(req,res) => {
+	res.render('projects.hbs',{
+		pageTitle: 'projects page'
+	})
+})
+
 app.get('/bad',(req,res) => {
 	res.send({
 		error: 'error'

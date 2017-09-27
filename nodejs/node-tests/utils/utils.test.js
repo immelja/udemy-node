6069de2228afe3d4630f86cc56a9fce74b05a30a
jaco <jaco.com>
2017-09-27 19:35:43 +1000

    applied formatting

--- nodejs/node-tests/utils/utils.test.js
+++ nodejs/node-tests/utils/utils.test.js
@@ -1,9 +1,9 @@
 const utils = require('./utils')
 
-it('should add two numbers',() => {
-  var res = utils.add(33,11)
+it('should add two numbers', () => {
+  var res = utils.add(33, 11)
 
   if (res !== 44) {
-      throw new Error(`Expected 44 gor ${res}`)
+    throw new Error(`Expected 44 gor ${res}`)
   }
 })
